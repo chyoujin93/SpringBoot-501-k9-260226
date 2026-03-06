ba060ed03458504e82e92868ad44c1d990ba18d2
lsy3709 <lsy3709@naver.com>
2026-03-06 10:15:01 +0900

    2교시, 스웨거 설정 및 확인 1

---
+++ src/main/java/com/busanit501/springboot0226/config/CustomServletConfig.java
@@ -0,0 +1,9 @@
+package com.busanit501.springboot0226.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@Configuration
+@EnableWebMvc
+public class CustomServletConfig {
+}
